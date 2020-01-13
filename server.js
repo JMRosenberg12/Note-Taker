9d394ea6afa5a9f4a41875cbc877cae25408921a
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-01-13 09:17:21 -0600

    edit server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
 
 app.get("/", function (req, res) {
@@ -16,6 +17,10 @@ app.get("/", function (req, res) {
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+//  routing
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 app.get("/api/notes", function(req, res) {
     return res.json(db.json);
