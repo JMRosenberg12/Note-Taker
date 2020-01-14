5b6caee14977404fdaf5a7ef24eeec3fbaa41e09
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-01-14 09:25:17 -0600

    edit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-var notesData = require("../db/db.json");
+var notesData = require("/db/db.json");
 
 
 module.exports = function (app) {
@@ -41,7 +41,7 @@ module.exports = function (app) {
     
             notesData.splice(index, 1);
           console.log(notesData);
-          fs.writeFile("./db/db.json", JSON.stringify(notesData), (results, err) => {
+          fs.writeFile("/db/db.json", JSON.stringify(notesData), (results, err) => {
             if (err) throw err;
             res.json(results)
           });
